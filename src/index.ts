5db5119aa4b016523daaff3d08424c563530033d
rohan <95855114+ROHAN13498@users.noreply.github.com>
2024-07-05 12:06:01 +0530

    wss

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import { Redis } from "ioredis";
 import WebSocket from "ws";
-require('dotenv').config()
-
-import cors from "cors"
+import { Redis } from "ioredis";
+import cors from "cors";
+require('dotenv').config();
 
 interface CustomWebSocket extends WebSocket {
     userId?: string;
@@ -12,18 +11,22 @@ interface CustomWebSocket extends WebSocket {
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 const client = new Redis(process.env.REDIS_SERVER!);
 
-const server = app.listen(3000, () => {
-    console.log("App is listening on port 3000");
+// Create HTTP server
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
+
+// Create WebSocket server
 const wss = new WebSocket.Server({ server });
 
 // Hashmap to store userId and WebSocket instance
 const userSocketMap: { [userId: string]: CustomWebSocket } = {};
 
 wss.on("connection", async (ws: CustomWebSocket, req) => {
-    const userId = req.url ? new URL(req.url, 'http://localhost').searchParams.get('userId') : undefined;
+    const userId = req.url ? new URL(req.url, `http://${req.headers.host}`).searchParams.get('userId') : undefined;
     if (userId) {
         // Store userId in WebSocket instance
         ws.userId = userId;
@@ -37,7 +40,6 @@ wss.on("connection", async (ws: CustomWebSocket, req) => {
     }
 
     ws.on("message", async (message: string) => {
-        //console.log(`Received message from userId ${ws.userId}: ${message}`);
         const parsedMessage = JSON.parse(message);
         if (parsedMessage && parsedMessage.code && parsedMessage.language) {
             // Push code submission to Redis list
@@ -58,7 +60,7 @@ wss.on("connection", async (ws: CustomWebSocket, req) => {
 
 async function pushCodeSubmissionToQueue(userId: string | undefined, code: string, language: string) {
     if (!userId) return; // No userId, skip handling submission
-    console.log("pushing code",code)
+    console.log("Pushing code", code);
     const submission = JSON.stringify({ userId, code, language });
     await client.lpush("submissions", submission);
 }
@@ -68,22 +70,19 @@ app.get("/", (req, res) => {
     return res.json({ message: "Healthy server" });
 });
 
-app.get("/redis",async (req,res)=>{
-    const message=await client.brpop("submissions",0)
-    return res.json({message})
-})
+app.get("/redis", async (req, res) => {
+    const message = await client.brpop("submissions", 0);
+    return res.json({ message });
+});
 
 app.post("/output", async (req, res) => {
-    const { userId, output,error} = req.body;
+    const { userId, output, error } = req.body;
     const ws = userSocketMap[userId];
     if (ws && ws.readyState === WebSocket.OPEN) {
-        if(error){
-            console.log(req.body)
-            ws.send(error)
-        }
-        else{
-            console.log("output",output)
-            ws.send("\n"+output);
+        if (error) {
+            ws.send(error);
+        } else {
+            ws.send("\n" + output);
         }
         res.json({ success: true });
     } else {
